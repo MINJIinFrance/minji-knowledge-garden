3a1a1aedc8741840fd01673331adc71304b04396
Minji NAM <111258143+MINJIinFrance@users.noreply.github.com>
2026-07-24 08:31:00 +0900

    fix: package Astro output for Sites runtime

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import node from "@astrojs/node";
 
 export default defineConfig({
   site: process.env.SITE_URL,
+  output: "server",
+  adapter: node({ mode: "standalone" }),
   integrations: [react()]
 });
